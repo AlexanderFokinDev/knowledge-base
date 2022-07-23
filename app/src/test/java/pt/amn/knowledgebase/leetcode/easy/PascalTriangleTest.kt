6f42e20931be901b046b59e7ab84a08c6d3af821
Alexander Fokin <alexanderfokindev@gmail.com>
2022-07-23 16:11:29 +0300

    PascalTriangle class and test

---
+++ app/src/test/java/pt/amn/knowledgebase/leetcode/easy/PascalTriangleTest.kt
@@ -0,0 +1,33 @@
+package pt.amn.knowledgebase.leetcode.easy
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class PascalTriangleTest {
+
+    @Test
+    fun pascalTriangleTest1() {
+
+        val target = listOf(
+            listOf(1),
+            listOf(1,1),
+            listOf(1,2,1),
+            listOf(1,3,3,1),
+            listOf(1,4,6,4,1)
+        )
+
+        val solver = PascalTriangle(5)
+        assertArrayEquals(target.toTypedArray(), solver.getResult().toTypedArray())
+    }
+
+    @Test
+    fun pascalTriangleTest2() {
+
+        val target = listOf(
+            listOf(1)
+        )
+
+        val solver = PascalTriangle(1)
+        assertArrayEquals(target.toTypedArray(), solver.getResult().toTypedArray())
+    }
+}
