fa1c4ec0d86bfe98ca46b1ef2cb0fff445dce775
Aleksandr Fokin <alexanderfokindev@gmail.com>
2022-08-13 15:37:05 -0300

    Research of standard class methods: equals, hashCode, toString

---
+++ app/src/test/java/pt/amn/knowledgebase/research/ClassesStandartMethodsResearchTest.kt
@@ -0,0 +1,110 @@
+package pt.amn.knowledgebase.research
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ClassesStandartMethodsResearchTest {
+
+    @Test
+    fun `Two objects of the class are not equals, and its fields too`() {
+
+        val expected = false
+
+        val rs1 = ResearchSimpleClass(1, "ResearchSimpleClass 1")
+        val rs2 = ResearchSimpleClass(2, "ResearchSimpleClass 2")
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchSimpleClass, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(2, hashMap.size)
+    }
+
+    @Test
+    fun `Two objects of the class are not equals, but its fields are`() {
+
+        val expected = false
+
+        val rs1 = ResearchSimpleClass(1, "ResearchSimpleClass 1")
+        val rs2 = ResearchSimpleClass(1, "ResearchSimpleClass 1")
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchSimpleClass, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(2, hashMap.size)
+    }
+
+    @Test
+    fun `Two objects of the data class are not equals, and its fields too`() {
+
+        val expected = false
+
+        val rs1 = ResearchDataClass(1, "ResearchDataClass 1")
+        val rs2 = ResearchDataClass(2, "ResearchDataClass 2")
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchDataClass, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(2, hashMap.size)
+    }
+
+    @Test
+    fun `Two objects of the data class are equals, and its fields too`() {
+
+        val expected = true
+
+        val rs1 = ResearchDataClass(1, "ResearchDataClass 1")
+        val rs2 = ResearchDataClass(1, "ResearchDataClass 1")
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchDataClass, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(1, hashMap.size)
+    }
+
+    @Test
+    fun `Two objects of the override class are not equals, and its fields too`() {
+
+        val expected = false
+
+        val rs1 = ResearchSimpleClassWithOverrideMetods(1, "ResearchSimpleClassWithOverrideMetods 1")
+        val rs2 = ResearchSimpleClassWithOverrideMetods(2, "ResearchSimpleClassWithOverrideMetods 2")
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchSimpleClassWithOverrideMetods, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(2, hashMap.size)
+    }
+
+    @Test
+    fun `Two objects of the override class are not equals, but its fields are`() {
+
+        val expected = true
+
+        val rs1 = ResearchSimpleClassWithOverrideMetods(1, "ResearchSimpleClassWithOverrideMetods 1")
+        val rs2 = ResearchSimpleClassWithOverrideMetods(1, "ResearchSimpleClassWithOverrideMetods 1")
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchSimpleClassWithOverrideMetods, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(1, hashMap.size)
+    }
+
+}
