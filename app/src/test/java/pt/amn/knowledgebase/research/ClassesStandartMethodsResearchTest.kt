fa8327240afdd39decf05e29c2cd274f9bb5c21d
Aleksandr Fokin <alexanderfokindev@gmail.com>
2022-08-16 16:27:37 -0300

    Research of standard class methods: copy, hashCode for data class

--- app/src/test/java/pt/amn/knowledgebase/research/ClassesStandartMethodsResearchTest.kt
+++ app/src/test/java/pt/amn/knowledgebase/research/ClassesStandartMethodsResearchTest.kt
@@ -71,6 +71,29 @@ class ClassesStandartMethodsResearchTest {
         hashMap.put(rs1, 1)
         hashMap.put(rs2, 2)
         assertEquals(1, hashMap.size)
+
+        assertEquals(rs1.hashCode(), rs2.hashCode())
+
+        assertFalse(rs1 === rs2)
+    }
+
+    @Test
+    fun `Object of the data class and his copy are equals, and its fields too`() {
+
+        val expected = true
+
+        val rs1 = ResearchDataClass(1, "ResearchDataClass 1")
+        val rs2 = rs1.copy()
+        val actual = ClassesStandartMethodsResearch(rs1, rs2).getResult()
+
+        assertEquals(expected, actual)
+
+        val hashMap = hashMapOf<ResearchDataClass, Int>()
+        hashMap.put(rs1, 1)
+        hashMap.put(rs2, 2)
+        assertEquals(1, hashMap.size)
+
+        assertEquals(rs1.hashCode(), rs2.hashCode())
     }
 
     @Test
