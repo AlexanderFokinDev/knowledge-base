e0e3db904712835d4faa2f97ca8eda8dcffee831
Aleksandr Fokin <alexanderfokindev@gmail.com>
2022-09-09 15:02:04 -0300

    Data structures research. List and mutableList

--- app/src/test/java/pt/amn/knowledgebase/research/DataStructuresResearchTest.kt
+++ app/src/test/java/pt/amn/knowledgebase/research/DataStructuresResearchTest.kt
@@ -10,4 +10,54 @@ class DataStructuresResearchTest {
 
         assertNotEquals(5, DataStructuresResearch().arrayListResearch())
     }
+
+    @Test
+    fun listResearchTest() {
+
+        val testList = listOf<Int>()
+        assertEquals(0, testList.size)
+
+        // Can't do it
+        //testList.add()
+
+        // It must be predetermined
+        val testList1 = listOf(1, 3, 5, 7, 9)
+        assertEquals(5, testList1.size)
+        // Can't change
+        //testList1[3] = 3
+
+        // Can't do it
+        //testList.add()
+    }
+
+    @Test
+    fun mutableListResearchTest() {
+
+        val testList = mutableListOf<Int>()
+        assertEquals(0, testList.size)
+
+        // Can do it
+        testList.add(7)
+        assertEquals(1, testList.size)
+
+        testList.removeAt(0)
+        assertEquals(0, testList.size)
+
+        // It must be predetermined
+        val testList1 = mutableListOf(1, 3, 5, 7, 9)
+        assertEquals(5, testList1.size)
+        // Can change
+        testList1[3] = 3
+
+        testList1.map { element ->
+            element*3
+        }
+        println(testList1)
+
+        testList1.remove(3)
+        println(testList1)
+
+        // Can do it
+        testList1.add(8)
+    }
 }
