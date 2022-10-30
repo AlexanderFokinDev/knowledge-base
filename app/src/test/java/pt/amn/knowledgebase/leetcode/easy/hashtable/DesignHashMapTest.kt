dded784e066667bf98e0c4bfcc85bddc15137160
Aleksandr Fokin <alexanderfokindev@gmail.com>
2022-10-30 12:40:51 -0300

    Leetcode solved task. DesignHashMap

---
+++ app/src/test/java/pt/amn/knowledgebase/leetcode/easy/hashtable/DesignHashMapTest.kt
@@ -0,0 +1,13 @@
+package pt.amn.knowledgebase.leetcode.easy.hashtable
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class DesignHashMapTest {
+
+    @Test
+    fun designHashMapTest1() {
+
+        assertEquals(mapOf(1 to 1), DesignHashMap().getResult())
+    }
+}
